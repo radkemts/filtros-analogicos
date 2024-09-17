1a81fb684bfa6e56017bfd55519ce4196def9dc3
radkemts <mateusradke@hotmail.com>
2024-09-17 02:59:14 -0300

    First commit

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
+    $('.div-input-text input[type="text"]').on('input', function() {
+        let value = $(this).val();
+        let newValue = '';
+
+        if (value.length > 0 && value[0] === '-') {
+            newValue = '-';
+            value = value.slice(1);
+        }
+
+        let hasDot = false;
+        for (let char of value) {
+            if (char === '.' && !hasDot) {
+                newValue += '.';
+                hasDot = true;
+            } else if (!isNaN(char) && char !== ' ') {
+                newValue += char;
+            }
+        }
+
+        $(this).val(newValue);
+    });
     renderMathInElement(document.querySelector('.latex'));
 });
